f0bffcb7cc689b27e9c31647c48ba75b53530ca9
Wolfgang Braun <wb@scherer-software.de>
2015-05-06 13:54:49 +0200

    load CkToolsHelper

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -5,4 +5,16 @@ namespace Notifications\Controller;
 use \App\Controller\AppController as BaseController;
 
 class AppController extends BaseController {
+    /**
+     * Initialization hook method.
+     *
+     * Use this method to add common initialization code like loading components.
+     *
+     * @return void
+     */
+    public function initialize()
+    {
+		$this->helpers[] = 'CkTools.CkTools';
+        parent::initialize();
+    }
 }
