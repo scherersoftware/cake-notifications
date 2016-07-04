19bbda23770e8c9f393db42ead88ced7da7199f8
Michael Hoffmann <mh@scherer-software.de>
2016-07-04 16:14:57 +0200

    removed some obsolete stuff

--- src/Transport/Transport.php
+++ src/Transport/Transport.php
@@ -3,38 +3,11 @@ namespace Notifications\Transport;
 
 abstract class Transport
 {
-
-    /**
-     * An array mapping transports to their classes
-     *
-     * @var array
-     */
-    private static $__transports = [
-        'email' => 'Notifications\Notification\EmailNotification',
-        'sms' => 'Notifications\Notification\SmsNotification'
-    ];
-
     /**
      * Abstract sender method
      *
      */
     abstract public function sendNotification($notification);
-
-    /**
-     * Creates a Transport instance based on the given type
-     *
-     * @param string $type email/sms
-     * @return Transport
-     * @throws \InvalidArgumentException
-     */
-    public static function factory($type)
-    {
-        if (!isset($this->__transports[$type])) {
-            throw new \InvalidArgumentException("{$type} is not a valid transport");
-        }
-        $className = $this->__transports[$type];
-        return $transport;
-    }
 
     /**
      * Performs the before- or after send callback of the notification
