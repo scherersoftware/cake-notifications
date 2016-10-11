90b18ae0859bf5eb2a334eacc081a2ac49f0d212
Michael Hoffmann <mh@scherer-software.de>
2016-10-11 18:22:14 +0200

    Better return the notification object then void

--- src/Transport/EmailTransport.php
+++ src/Transport/EmailTransport.php
@@ -17,7 +17,7 @@ class EmailTransport extends Transport implements TransportInterface
      *
      * @param Notification $notification Notification object
      * @param string|array|null $content String with message or array with messages
-     * @return void
+     * @return Notification
      */
     public static function sendNotification(Notification $notification, $content = null)
     {
@@ -34,6 +34,8 @@ class EmailTransport extends Transport implements TransportInterface
 
         $afterSendCallback = $notification->afterSendCallback();
         self::_performCallback($afterSendCallback);
+
+        return $notification;
     }
 
     /**
