352794702e017ad682b65c2fe9b872556140cd33
Michael Hoffmann <mh@scherer-software.de>
2016-10-14 13:29:51 +0200

    some new transport tests

--- tests/TestCase/Transport/EmailTransportTest.php
+++ tests/TestCase/Transport/EmailTransportTest.php
@@ -2,7 +2,10 @@
 namespace Notifications\Test\TestCase\Notification;
 
 use Cake\I18n\I18n;
+use Cake\Log\Log;
+use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
+use Josegonzalez\CakeQueuesadilla\Queue\Queue;
 use Notifications\Notification\EmailNotification;
 use Notifications\Transport\EmailTransport;
 
@@ -13,12 +16,25 @@ use Notifications\Transport\EmailTransport;
 class SomeClass
 {
 
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'Jobs' => 'plugin.notifications.jobs'
+    ];
+
     public static $someProperty = null;
     public static $anotherProperty = null;
+    public static $wasTheCallableCalled = false;
 
     public function someMethod()
     {
         self::$someProperty = 'was_called';
+        return function (EmailNotification $email) {
+            self::$wasTheCallableCalled = true;
+        };
     }
 
     public static function someStaticMethod()
@@ -29,6 +45,54 @@ class SomeClass
 
 class EmailTransportTest extends TestCase
 {
+
+    public function setup()
+    {
+        parent::setUp();
+        Log::reset();
+        Log::config('stdout', ['engine' => 'File']);
+
+        $dbConfig = \Cake\Datasource\ConnectionManager::config('test');
+
+        Queue::reset();
+        Queue::config([
+            'default' => [
+                'engine' => 'josegonzalez\Queuesadilla\Engine\MysqlEngine',
+                'database' => $dbConfig['database'],
+                'host' => $dbConfig['host'],
+                'user' => $dbConfig['username'],
+                'pass' => $dbConfig['password']
+            ]
+        ]);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        Log::reset();
+        Queue::reset();
+    }
+
+    public function testNotExistingCallbackCall()
+    {
+        $email = new EmailNotification([
+            'transport' => 'debug',
+            'from' => 'foo@bar.com'
+        ]);
+        $email->to('foo@bar.com')
+            ->beforeSendCallback(['SomeClassThatDoesNotExists', 'someMethod']);
+        $return = EmailTransport::sendNotification($email);
+
+        $this->assertEquals(SomeClass::$someProperty, null);
+        $this->assertEquals(SomeClass::$anotherProperty, null);
+        $this->assertFalse(SomeClass::$wasTheCallableCalled);
+    }
 
     public function testSendNotification()
     {
@@ -46,12 +110,11 @@ class EmailTransportTest extends TestCase
 
         $this->assertEquals(SomeClass::$someProperty, 'was_called');
         $this->assertEquals(SomeClass::$anotherProperty, 'was_called');
+        $this->assertTrue(SomeClass::$wasTheCallableCalled);
     }
 
     public function testProcessQueueObject()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+
     }
 }
